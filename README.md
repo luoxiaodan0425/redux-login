234a5b6886a8d95eef0b250271ef88681838312e
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-05-17 17:33:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### React & Redux 实现注册登录认证系统
+
+视频请看：https://www.rails365.net/playlists/react-redux-shi-xian-zhu-ce-deng-lu-ren-zheng-xi-tong
+
+### 联系我们
+
+qq群：697272886
---
+++ client/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavigationBar from './components/NavigationBar';
+import FlashMessagesList from './components/flash/FlashMessagesList';
+import registerServiceWorker from './registerServiceWorker';
+
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import routes from './routes';
+
+import { createStore, applyMiddleware } from 'redux';
+
+import rootReducer from './reducers';
+
+import { Provider } from 'react-redux';
+
+import jwtDecode from 'jwt-decode';
+
+import setAuthorizationToken from './utils/setAuthorizationToken';
+import { setCurrentUser } from './actions/authActions';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk, logger)
+  )
+);
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+}
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <Router routes={ routes }>
+      <div>
+        <NavigationBar />
+        <FlashMessagesList />
+        { routes }
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
